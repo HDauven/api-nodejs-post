81b06c4221761693bf1c604ea9fb43205f75a0fb
Hein Dauven <heindauven@gmail.com>
2019-01-02 04:07:25 +0100

    Add posts via database instead of test endpoint.

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require("express-validator/check");
 
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -18,23 +20,28 @@ exports.getPosts = (req, res, next) => {
 exports.postPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: "Validation failed, entered data is incorrect.",
-        errors: errors.array()
-      });
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect.",
+      errors: errors.array()
+    });
   }
 
   const { title, content } = req.body;
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      _id: new Date().toISOString(),
-      title,
-      content,
-      creator: { name: "Max" },
-      createdAt: new Date()
-    }
+  const post = new Post({
+    title,
+    content,
+    imageUrl: "images/fred.png",
+    creator: { name: "Max" }
   });
+  post
+    .save()
+    .then(result => {
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
