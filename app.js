b40407c6f1e7417baf656e9e864af747b86ecc3e
Hein Dauven <heindauven@gmail.com>
2019-01-04 15:17:06 +0100

    Create wrapper around socket.io to make it accessible across the application.

--- app.js
+++ app.js
@@ -63,6 +63,10 @@ const MONGODB_URI = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/messages`;
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require("./socket").init(server);
+    io.on("connection", socket => {
+      console.log("Client connected");
+    });
   })
   .catch(err => console.log(err));
