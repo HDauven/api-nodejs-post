609aa0bc8209559b3e2867b420f124d5c12fbdd9
Hein Dauven <heindauven@gmail.com>
2019-01-05 02:31:35 +0100

    Setup Graphql

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const multer = require("multer");
+const graphqlHttp = require("express-graphql");
 
-const feedRoutes = require("./routes/feed");
-const authRoutes = require("./routes/auth");
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolver = require("./graphql/resolvers");
+
+// const feedRoutes = require("./routes/feed");
+// const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -47,8 +51,15 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/feed", feedRoutes);
-app.use("/auth", authRoutes);
+// app.use("/feed", feedRoutes);
+// app.use("/auth", authRoutes);
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver
+  })
+);
 
 app.use((error, req, res, next) => {
   console.log(error);
@@ -64,9 +75,9 @@ mongoose
   .connect(MONGODB_URI)
   .then(result => {
     const server = app.listen(8080);
-    const io = require("./socket").init(server);
-    io.on("connection", socket => {
-      console.log("Client connected");
-    });
+    // const io = require("./socket").init(server);
+    // io.on("connection", socket => {
+    //   console.log("Client connected");
+    // });
   })
   .catch(err => console.log(err));
